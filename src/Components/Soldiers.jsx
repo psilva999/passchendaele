266dd284e8293195572ac886b4e98d5333e9519a
PSILVA999 <p.silva0399@gmail.com>
2023-07-20 10:50:02 -0300

    mousedown, mousemove and mouseup events on slide

--- src/Components/Soldiers.jsx
+++ src/Components/Soldiers.jsx
@@ -1,43 +1,59 @@
-import fig1 from '../assets/images/slide/fig1.jpg'
-// import fig2 from '../assets/images/slide/fig2.jpg'
+import { useEffect } from 'react'
 
-// import fig3 from '../assets/images/slide/fig3.jpg'
-// import fig4 from '../assets/images/slide/fig4.jpg'
-// import fig5 from '../assets/images/slide/fig5.jpg'
+import fig1 from '../assets/images/slide/fig1.jpg'
+import fig2 from '../assets/images/slide/fig2.jpg'
+
+import fig3 from '../assets/images/slide/fig3.jpg'
+import fig4 from '../assets/images/slide/fig4.jpg'
+import fig5 from '../assets/images/slide/fig5.jpg'
+
+import Slide from '../events/slide'
 
 const Soldiers = () => {
+  useEffect(() => {
+    //mostrando quais containers serão utilizados
+    const handleSlide = new Slide('.slide', '.slide-wrapper')
+
+    //inicia o slide
+    handleSlide.init()
+
+    console.log(handleSlide)
+  })
+
   return (
 
     <section id='soldiers'>
       <h1>Soldiers</h1>
 
-      <div className='slide'>
-        <figure> <img src={ fig1 } alt="Canadian prisioners"/>
+      <article className='slide-wrapper'>
+        <div className='slide'>
+          <figure> <img src={ fig1 } alt="Canadian prisioners"/>
 
-          <figcaption>Canadian prisioners in Passchendaele</figcaption>
-        </figure>
+            <figcaption>Canadian prisioners in Passchendaele</figcaption>
+          </figure>
 
-        {/* <figure> <img src={ fig2 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig2 } alt="Canadian prisioners"/>
 
-          <figcaption>The price of a mile</figcaption>
-        </figure>
+            <figcaption>The price of a mile</figcaption>
+          </figure>
 
-        <figure> <img src={ fig3 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig3 } alt="Canadian prisioners"/>
 
-          <figcaption>Dreams of freedom turn to dust</figcaption>
-        </figure>
+            <figcaption>Dreams of freedom turn to dust</figcaption>
+          </figure>
 
-        <figure> <img src={ fig4 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig4 } alt="Canadian prisioners"/>
 
-          <figcaption>How many wasted their lives</figcaption>
-        </figure>
+            <figcaption>How many wasted their lives</figcaption>
+          </figure>
 
-        <figure> <img src={ fig5 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig5 } alt="Canadian prisioners"/>
 
-          <figcaption>How many dreams fade away</figcaption>
-        </figure> */}
+            <figcaption>How many dreams fade away</figcaption>
+          </figure>
 
-      </div>
+        </div>
+      </article>
     </section>
 
   )
--- src/events/slide.js
+++ src/events/slide.js
@@ -1,1 +1,59 @@
+//export da class principal
+export default class Slide {
 
+  //constructor do slide
+  constructor(slide, wrapper) {
+    //chamando os containers com o 'this'
+    this.slide = document.querySelector(slide)
+    this.wrapper = document.querySelector(wrapper)
+  }
+
+  //remover evento padrao
+  onStart(e) {
+    e.preventDefault()
+    console.log('mousedown')
+
+    //so move quando iniciar
+    this.wrapper.addEventListener('mousemove', this.onMove)
+  }
+
+  //quando mover o slide
+  onMove() {
+    console.log('moveu')
+  }
+
+  //quando terminar o evento
+  onEnd() {
+    console.log("cabou")
+    //remove mousemove quando acabar
+    this.wrapper.removeEventListener('mousemove', this.onMove)
+  }
+
+  //para adicionar cada evento do slide
+  addSlideEvents() {
+    //evento de mouse down
+    this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('mouseup', this.onEnd)
+  }
+
+  //bind events para fazer referencia ao objeto
+  bindEvents() {
+    //impede que retorne o elemento quando tiver mousedown
+    this.onStart = this.onStart.bind(this)
+    this.onMove = this.onMove.bind(this)
+
+    this.onEnd = this.onEnd.bind(this)
+  }
+
+  //quando o slide iniciar
+  init() {
+    //começando pelo bind
+    this.bindEvents()
+    
+    //depois os outros eventos
+    this.addSlideEvents()
+
+    //retorna a funçao addSlideEvents
+    return this
+  }
+}
