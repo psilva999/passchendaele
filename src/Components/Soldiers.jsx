c1ccc49a33d781a81a209a382a8e54fce06c54a5
PSILVA999 <p.silva0399@gmail.com>
2023-07-22 11:55:06 -0300

    new images

--- src/Components/Soldiers.jsx
+++ src/Components/Soldiers.jsx
@@ -2,10 +2,11 @@ import { useEffect } from 'react'
 
 import fig1 from '../assets/images/slide/fig1.jpg'
 import fig2 from '../assets/images/slide/fig2.jpg'
-
 import fig3 from '../assets/images/slide/fig3.jpg'
+
 import fig4 from '../assets/images/slide/fig4.jpg'
 import fig5 from '../assets/images/slide/fig5.jpg'
+import fig6 from '../assets/images/slide/fig6.jpg'
 
 import Slide from '../events/slide'
 
@@ -26,29 +27,34 @@ const Soldiers = () => {
 
       <article className='slide-wrapper'>
         <div className='slide'>
-          <figure> <img src={ fig1 } alt="Canadian prisioners"/>
-
-            <figcaption>Canadian prisioners in Passchendaele</figcaption>
-          </figure>
-
-          <figure> <img src={ fig2 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig1 } alt="Price"/>
 
             <figcaption>The price of a mile</figcaption>
           </figure>
 
-          <figure> <img src={ fig3 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig2 } alt="tank"/>
 
             <figcaption>Dreams of freedom turn to dust</figcaption>
           </figure>
 
-          <figure> <img src={ fig4 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig3 } alt="fade"/>
+
+            <figcaption>An army in dispair</figcaption>
+          </figure>
+
+          <figure> <img src={ fig4 } alt="away"/>
 
             <figcaption>How many wasted their lives</figcaption>
           </figure>
 
-          <figure> <img src={ fig5 } alt="Canadian prisioners"/>
+          <figure> <img src={ fig5 } alt="broken"/>
 
             <figcaption>How many dreams fade away</figcaption>
+          </figure>
+
+          <figure> <img src={ fig6 } alt="dreams"/>
+
+            <figcaption>They wont be coming home</figcaption>
           </figure>
 
         </div>
